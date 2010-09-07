82111ad2cedd218fbd91c4d68168a6fb1fb611a7
rech.christian <rech.christian@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-07 10:45:04 +0000

    Listen ansicht erstellt

---
+++  muvibee/MuViBee.Backend/src/de/muvibee/playground/list/ListView.java
@@ -0,0 +1,51 @@
+package de.muvibee.playground.list;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+
+public class ListView extends JPanel{
+	DefaultListModel  listModel;
+	CellRenderer lcr;
+	JList list;
+	
+	public ListView() {
+			setLayout(new BorderLayout());
+		
+			listModel = new DefaultListModel();
+			lcr = new CellRenderer();
+ 	        list = new JList(listModel);
+ 	        list.setCellRenderer(lcr);
+ 	        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+ 	        list.setLayoutOrientation(JList.VERTICAL);
+ 	        list.setVisibleRowCount(10);
+ 	        
+	        list.addListSelectionListener(new ListSelectionListener() {
+	        	public void valueChanged(ListSelectionEvent evt){
+	        		System.out.println("Feld: " + list.getSelectedIndex() + " " + (evt.getValueIsAdjusting()?"PREP":"SEL"));
+	        	}
+	        });
+ 	        
+	        JScrollPane pane = new JScrollPane(list);
+	        pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+			pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+	        pane.setPreferredSize(new Dimension(150,600));
+	        
+	        add(pane, BorderLayout.NORTH);
+	}
+	
+	
+	public void listAdd(ListEntry entry){
+		listModel.addElement(entry);
+		list.validate();
+	}
+}
+
---
+++  muvibee/MuViBee.Backend/src/de/muvibee/playground/list/UI.java
@@ -0,0 +1,64 @@
+package de.muvibee.playground.list;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class UI extends JFrame{
+	ListView listView;
+	
+	public UI() {
+		super("User Interface");
+
+		// ContentPane setzen:
+//		JPanel content = new JPanel(new GridBagLayout());
+		listView = new ListView();;
+		JPanel content = listView;
+		this.setContentPane(content);
+		
+		//button
+		JButton addButton = new JButton("Add Entry");
+		addButton.setSize(12, 24);
+		addButton.addActionListener(new ActionListener() {
+			int i = 1;
+			public void actionPerformed(ActionEvent e) {
+				addItem(i  + ".jpg","Author" + i,"Titel" + i);
+				i++;
+            }
+        });
+		
+		content.add(addButton);
+
+		GridBagConstraints c = new GridBagConstraints();
+		
+        // Groesse des Fensterbereichs setzen:
+        content.setPreferredSize(new Dimension(250, 650));
+
+        // Icon setzen:		// getClass().getResource() sucht die Resource (hier muvibee.gif im Unterverzeichnis icons) relativ zum Pfad der Class
+//        this.setIconImage(new ImageIcon(getClass().getResource(".\\icons\\muvibee.gif")).getImage());
+
+        // Programm beenden, wenn das Fenster geschlossen wird:
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private void addItem(String cover, String author, String title) {
+		listView.listAdd(new ListEntry(cover, author, title));
+	}
+	
+	public static void main(String[] args) {
+		UI ui = new UI();
+		ui.pack();
+		ui.setLocationRelativeTo(null);
+		ui.setVisible(true);
+	}
+
+	
+}
