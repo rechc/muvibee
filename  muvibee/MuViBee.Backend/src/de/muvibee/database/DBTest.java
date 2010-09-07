60179e82432c93ffb8b744fc1cfcf8b6df6c9346
yassir.klos <yassir.klos@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-07 10:08:53 +0000

    asd

---
+++  muvibee/MuViBee.Backend/src/de/muvibee/database/DBTest.java
@@ -0,0 +1,47 @@
+package de.muvibee.database;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBTest {
+	final static String SQL1 = "CREATE TABLE IF NOT EXISTS `infos` (`ID` int(10) NOT NULL AUTO_INCREMENT,`mediaID` int(10) DEFAULT NULL,`k_infoID` int(10) DEFAULT NULL,`value` text COLLATE latin1_german2_ci,PRIMARY KEY (`ID`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1";
+	final static String SQL2 = "CREATE TABLE IF NOT EXISTS `k_format` (`ID` int(10) NOT NULL AUTO_INCREMENT,`mediaID` int(10) DEFAULT NULL,`stxt` varchar(255) COLLATE latin1_german2_ci DEFAULT NULL,`ltxt` text COLLATE latin1_german2_ci,PRIMARY KEY (`ID`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1";
+	final static String SQL3 = "CREATE TABLE IF NOT EXISTS `k_genre` (`ID` int(10) NOT NULL AUTO_INCREMENT,`stxt` varchar(255) COLLATE latin1_german2_ci DEFAULT NULL,`ltxt` text COLLATE latin1_german2_ci,PRIMARY KEY (`ID`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1";
+	final static String SQL4 = "CREATE TABLE IF NOT EXISTS `k_infos` (`ID` int(10) NOT NULL AUTO_INCREMENT,`infosID` int(10) DEFAULT NULL,`name` varchar(45) COLLATE latin1_german2_ci DEFAULT NULL,PRIMARY KEY (`ID`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1";
+	final static String SQL5 = "CREATE TABLE IF NOT EXISTS `lend` (`ID` int(10) NOT NULL AUTO_INCREMENT,`mediaID` int(10) DEFAULT NULL,`lendDate` date DEFAULT NULL,`backDate` date DEFAULT NULL,`lendTo` varchar(255) COLLATE latin1_german2_ci DEFAULT NULL,PRIMARY KEY (`ID`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1";
+	final static String SQL6 = "CREATE TABLE IF NOT EXISTS `media` (`ID` int(10) NOT NULL AUTO_INCREMENT,`infosID` int(10) DEFAULT NULL,`k_genreID` int(10) DEFAULT NULL,`lendID` int(10) DEFAULT NULL,`k_formatID` int(10) DEFAULT NULL,`title` varchar(255) COLLATE latin1_german2_ci DEFAULT NULL,`description` text COLLATE latin1_german2_ci,`cover` varchar(255) COLLATE latin1_german2_ci DEFAULT NULL,`rating` int(10) DEFAULT NULL,`comment` varchar(255) COLLATE latin1_german2_ci DEFAULT NULL,`location` varchar(255) COLLATE latin1_german2_ci DEFAULT NULL,`EAN` int(10) DEFAULT NULL,`year` int(10) DEFAULT NULL,`genreID` int(10) DEFAULT NULL,`isDeleted` tinyint(1) NOT NULL,PRIMARY KEY (`ID`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1";
+	
+	public static void main(String[] args) {
+		try {
+			PreparedStatement ps1 = DBConnector.getConnection().prepareStatement(SQL1);
+			PreparedStatement ps2 = DBConnector.getConnection().prepareStatement(SQL2);
+			PreparedStatement ps3 = DBConnector.getConnection().prepareStatement(SQL3);
+			PreparedStatement ps4 = DBConnector.getConnection().prepareStatement(SQL4);
+			PreparedStatement ps5 = DBConnector.getConnection().prepareStatement(SQL5);
+			PreparedStatement ps6 = DBConnector.getConnection().prepareStatement(SQL6);
+			
+			System.out.println("0");
+			ps1.executeUpdate();
+			System.out.println("1");
+			
+			ps2.executeUpdate();
+			System.out.println("2");
+			
+			ps3.executeUpdate();
+			System.out.println("3");
+			
+			ps4.executeUpdate();
+			System.out.println("4");
+			
+			ps5.executeUpdate();
+			System.out.println("5");
+			
+			ps6.executeUpdate();
+			System.out.println("6");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
