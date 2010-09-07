4039472f242e300cd1191f9d15bd2b94ab72fa47
altmeyer.thomas <altmeyer.thomas@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-07 15:46:26 +0000

    Testklasse

---
+++ trunk/ muvibee/MuViBee.Backend/src/de/muvibee/ean/Test.java
@@ -0,0 +1,13 @@
+package de.muvibee.ean;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EAN ean = new EAN();
+		ean.searchEan("97839807-10688");
+	}
+}
