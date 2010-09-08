76e3a7eb16c11672dec192fb18e92dbd2612e079
altmeyer.thomas <altmeyer.thomas@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-08 09:08:27 +0000

    Spracherweiterung fixed

---  muvibee/MuViBee.Backend/src/de/muvibee/ean/Test.java
+++  muvibee/MuViBee.Backend/src/de/muvibee/ean/Test.java
@@ -10,11 +10,11 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Locale turkish = new Locale("tr");
-		Locale russian = new Locale("ru");
+		Locale turkish = new Locale("tr", "TR");
+		Locale russian = new Locale("ru", "RU");
 		//Kann man die Sprache setzen, wenn nicht wird die Systemsprache verwendet
 		//Locale.setDefault(russian);
 		EAN ean = new EAN();
-		ean.searchEan("97839807-10688");
+		ean.searchEan("97839807-106788");
 	}
 }
