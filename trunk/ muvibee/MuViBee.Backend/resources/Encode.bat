da8bbfe72e7251cf39089cc5ed71660297e8c0aa
altmeyer.thomas <altmeyer.thomas@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-08 12:10:16 +0000

    Sprachunterstützung

--- trunk/ muvibee/MuViBee.Backend/resources/Encode.bat
+++ trunk/ muvibee/MuViBee.Backend/resources/Encode.bat
@@ -1,4 +1,8 @@
+del .\MuViBee.properties
+del .\MuViBee_de.properties
 del .\MuViBee_ru.properties
 del .\MuViBee_tr.properties
-%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_ru_init.properties .\MuViBee_ru.properties
-%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_tr_init.properties .\MuViBee_tr.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\init_language\MuViBee_init.properties .\MuViBee.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\init_language\MuViBee_de_init.properties .\MuViBee_de.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\init_language\MuViBee_ru_init.properties .\MuViBee_ru.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\init_language\MuViBee_tr_init.properties .\MuViBee_tr.properties
