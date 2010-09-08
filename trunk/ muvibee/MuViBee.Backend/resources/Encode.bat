48c37b8d42ba0a765f4bb49ae305b3e7111925ab
altmeyer.thomas <altmeyer.thomas@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-08 09:08:27 +0000

    Spracherweiterung fixed

--- trunk/ muvibee/MuViBee.Backend/resources/Encode.bat
+++ trunk/ muvibee/MuViBee.Backend/resources/Encode.bat
@@ -1,2 +1,4 @@
+del .\MuViBee_ru.properties
+del .\MuViBee_tr.properties
 %JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_ru.txt .\MuViBee_ru.properties
-%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_tu.txt .\MuViBee_tu.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_tr.txt .\MuViBee_tr.properties
