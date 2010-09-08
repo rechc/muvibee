a82afe065ea3a64527bec74a0763d8b392420084
altmeyer.thomas <altmeyer.thomas@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-08 08:50:17 +0000

    Spracherweiterung

---
+++ trunk/ muvibee/MuViBee.Backend/resources/Encode.bat
@@ -0,0 +1,2 @@
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_ru.txt .\MuViBee_ru.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_tu.txt .\MuViBee_tu.properties
--- trunk/ muvibee/MuViBee.Backend/src/de/muvibee/ean/Test.java
+++ trunk/ muvibee/MuViBee.Backend/src/de/muvibee/ean/Test.java
@@ -1,5 +1,8 @@
 package de.muvibee.ean;
 
+import java.util.Locale;
+
+
 
 public class Test {
 
@@ -7,6 +10,10 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Locale turkish = new Locale("tr");
+		Locale russian = new Locale("ru");
+		//Kann man die Sprache setzen, wenn nicht wird die Systemsprache verwendet
+		//Locale.setDefault(russian);
 		EAN ean = new EAN();
 		ean.searchEan("97839807-10688");
 	}
