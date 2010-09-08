ae6382b0046e2afdc78ae8fee0aafa11ebc6097b
altmeyer.thomas <altmeyer.thomas@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-08 11:29:32 +0000

    Verbesserungen für Sprachunterstützung

--- trunk/ muvibee/MuViBee.Backend/resources/Encode.bat
+++ trunk/ muvibee/MuViBee.Backend/resources/Encode.bat
@@ -1,4 +1,4 @@
 del .\MuViBee_ru.properties
 del .\MuViBee_tr.properties
-%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_ru.txt .\MuViBee_ru.properties
-%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_tr.txt .\MuViBee_tr.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_ru_init.properties .\MuViBee_ru.properties
+%JAVA_HOME%\bin\native2ascii -encoding UTF8 .\MuViBee_tr_init.properties .\MuViBee_tr.properties
