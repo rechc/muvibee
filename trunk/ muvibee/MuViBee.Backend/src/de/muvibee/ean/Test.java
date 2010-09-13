1883a9893eefb73dc7324beb473e9b5540861aac
tobias.lana <tobias.lana@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-13 13:51:11 +0000

    sysouts

--- trunk/ muvibee/MuViBee.Backend/src/de/muvibee/ean/Test.java
+++ trunk/ muvibee/MuViBee.Backend/src/de/muvibee/ean/Test.java
@@ -17,15 +17,21 @@ public class Test {
 		EAN ean = new EAN();
 		//Illuminati Buch EAN
 		ean.searchEan("9783404148660");
+		System.out.println("-------------");
 		//Schindlers Liste Buch
 		ean.searchEan("9783570300046");
+		System.out.println("-------------");
 		// Schindlers Liste Movie
 		ean.searchEan("5050582207804");
+		System.out.println("-------------");
 		// Beverly Hills Cop
 		ean.searchEan("4010884538366");
+		System.out.println("-------------");
 		// Bravo Hits 65 CD
 		ean.searchEan("0886974796626");
+		System.out.println("-------------");
 		// Hellboy DVD
 		ean.searchEan("5050582594218");
+		System.out.println("-------------");
 	}
 }
