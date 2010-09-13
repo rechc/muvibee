aa285f4c003a1ad7f8eee1d5a7da6120efdc8bfb
tobias.lana <tobias.lana@22d6d1c2-b7dd-dad4-1a09-f4ff3511c7bc>
2010-09-13 13:16:36 +0000

    Auslesen der eingefuegten Daten in TestDBSelects ausgelagert

---  muvibee/MuViBee.Backend/src/trash/TestDBInserts.java
+++  muvibee/MuViBee.Backend/src/trash/TestDBInserts.java
@@ -16,9 +16,10 @@ import de.muvibee.util.DBSelector;
 public class TestDBInserts {
 
 	public static void main(String[] args) {
-		Book b = new Book("null", "null", "null", "einBuch", "null", "null", 0, "null", "null", "null", "null", 0, "null", "null", null, false);
-		Music m = new Music("null", "null", "null", "ichMusik", "null", "null", 0, "null", "null", "null", "null", 0, "null", "null", null, false);
-		Video v = new Video("null", "null", "null", "DuVideo", "null", "null", 0, "null", "null", "null", "null", 0, "null", "null", null, false);
+		
+		Book b = new Book("null", "null", "null", "einBuch23", "null", "null", 0, "null", "null", "null", "null", 0, "null", "null", null, false);
+		Music m = new Music("null", "null", "null", "ichMusik23", "null", "null", 0, "null", "null", "null", "null", 0, "null", "null", null, false);
+		Video v = new Video("null", "null", "null", "DuVideo23", "null", "null", 0, "null", "null", "null", "null", 0, "null", "null", null, false);
 		BufferedImage bi1 = new BufferedImage(1, 2, 3);
 		BufferedImage bi2 = new BufferedImage(4, 5, 6);
 		BufferedImage bi3 = new BufferedImage(7, 8, 9);
@@ -31,31 +32,16 @@ public class TestDBInserts {
 		m.setCover(bi2);
 		m.setBackDate("2010-10-10");
 		m.setLendDate("2010-10-10");
+		System.out.println(m.toString());
 		m.insertIntoDB();
 		
 		v.setCover(bi3);
 		v.setBackDate("2010-10-10");
 		v.setLendDate("2010-10-10");
+		System.out.println(v.toString());
 		v.insertIntoDB();	
-		
-		
-//		try {
-//			Connection con = null;
-//			con = DBConnector.getConnection();
-//			String insert_sql = "INSERT INTO books (title) VALUES ('ichbineinBuch')";
-//			Statement st = con.createStatement();
-//			st.execute(insert_sql);
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 
 		
-		DBSelector dbs = new DBSelector("books", false);
-		LinkedList<Media> mediaList;
-		mediaList = dbs.getMediaList();
-		for (Media m1 : mediaList) {
-			m1.toString();
-		}
+		
 	}
 }
